ce92d675f34bc95f6331eda2541dddb642627751
ahmet aykut ay <ahmetaykutay@ahmets-MacBook-Air.local>
2019-02-11 17:36:50 +0300

    app styles

--- src/containers/Page/styles.js
+++ src/containers/Page/styles.js
@@ -1,8 +1,11 @@
 export default {
   wrapper: {
-    backgroundColor: '#ccc',
+    backgroundColor: '#eee',
     width: '100%',
     height: '100%',
-    overflow: 'scroll'
+    overflowY: 'scroll',
+    overflowX: 'hidden',
+    display: 'flex',
+    justifyContent: 'center'
   }
 }
