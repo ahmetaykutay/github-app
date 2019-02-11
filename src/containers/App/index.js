0757a0608f1b3c5de40f6dbc76910b69690c3f77
ahmet aykut ay <ahmetaykutay@ahmets-MacBook-Air.local>
2019-02-11 16:26:29 +0300

    material ui added

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Home, NotFound } from '../../pages'
+import Navbar from '../Navbar'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter basename="/">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/notfound" component={NotFound} />
-          <Redirect to="/notfound" />
-        </Switch>
+        <>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/notfound" component={NotFound} />
+            <Redirect to="/notfound" />
+          </Switch>
+        </>
       </BrowserRouter>
     )
   }
---
+++ src/containers/Navbar/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Github';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 'auto',
+  },
+  link: {
+    color: '#ccc'
+  },
+  linkActive: {
+    color: '#fff'
+  }
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar style={{backgroundColor: '#363636'}} position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Button className={classes.link} color="inherit">Search Repos</Button>
+          <Button className={classes.link} color="inherit">Search Organizations</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
